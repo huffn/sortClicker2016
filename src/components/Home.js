bd365a8e55d48d761d7104ea630dc0a105e8e13d
Nathan Huff <huffn@ldschurch.org>
2016-10-11 13:14:08 -0600

    Adding authentication

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,15 @@ import HighScores from './HighScores';
 import Game from './Game';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.authenticate = this.authenticate.bind(this);
+    this.authHandler = this.authHandler.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+  
   state = {
+    uid: null,
     highScores: {}
   }
   
@@ -16,12 +24,61 @@ class Home extends Component {
     });
   }
   
+  authenticate(provider) {
+    base.authWithOAuthPopup(provider, this.authHandler);
+  }
+  
+  authHandler(err, authData) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const uid = authData.user.uid
+    const userData = {
+      avatar: authData.user.photoURL,
+      name: authData.user.displayName,
+      score: 0
+    }
+    const highScoreRef = base.database().ref(`highScores/${uid}`);
+    
+    highScoreRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {};
+      
+      if(!data.score) {
+        highScoreRef.set(userData);
+      } else {
+        userData.score = data.score;
+      }
+    });
+    this.setState({
+      uid: authData.user.uid
+    });
+  }
+  
+  logout() {
+    base.unauth();
+    this.setState({ uid: null });
+  }
+  
   render() {
+    const logo = <Logo title="SORT Clicker"/>
+    if(!this.state.uid) {
+      return (
+        <div>
+          {logo}
+          <div className="login">
+            <a href="#" onClick={() => this.authenticate('github')}>Log In with Github</a>
+            <a href="#" onClick={() => this.authenticate('google')}>Log In with Google</a>
+          </div>
+        </div>
+      )
+    }
     return (
       <div className="fullScreen">
-        <Logo title="SORT Clicker"/>
+        {logo}
         <div className="playArea">
           <div className="gameArea">
+            <a href="#" onClick={this.logout}>Log Out</a>
             <Game />
           </div>
           <HighScores scores={this.state.highScores}/>
