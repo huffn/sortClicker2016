a9f2688ecf461d76bd47ca0d9ca99fc2af5a7c9f
Nathan Huff <huffn@ldschurch.org>
2016-10-11 11:29:54 -0600

    Getting state set up

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAQy_WLfyb55uiVtM6zPxmwnWaXtz7CpT8",
+  authDomain: "sort2016clicker.firebaseapp.com",
+  databaseURL: "https://sort2016clicker.firebaseio.com",
+});
+
+export default base;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import base from '../base';
 import Logo from './Logo';
 
 class Home extends Component {
+  state = {
+    highScores: {}
+  }
+  
+  componentWillMount() {
+    this.ref = base.syncState('highScores', {
+      context: this,
+      state: 'highScores'
+    });
+  }
+  
   render() {
     return (
       <Logo title="SORT Clicker"/>
